c2442237864e5560e4476c97ffea37398ce7a267
Brian-bot-afk <80060597+Brian-bot-afk@users.noreply.github.com>
2023-01-13 08:27:43 +0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,32 @@
+<?php
+require "header.php";
+?>
+<!DOCTYPE html>
+<html>
+<header class="header">
+    <div class="row">
+        <div class="col-md-12 text-center">
+   <a class="logo"><img src="img/logo.jpg" alt="logo"></a>
+   </div>
+        <div class="col-md-12 text-center">
+            <button type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-light btn-lg"><em>Make a Reservation Now!</em></button>
+        </div>
+    </div>
+</header>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1 {background-color: #4CAF50;} /* Green */
+.button2 {background-color: #008CBA;} /* Blue */
+</style>
+</head>
